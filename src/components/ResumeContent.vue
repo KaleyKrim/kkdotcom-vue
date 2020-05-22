d3de79abeeb2870b8240bfb6a12168cdffe4d546
Kaley Kriminger <kaley.kriminger@gmail.com>
2020-05-21 22:15:19 -0700

    add component for main resume content

---
+++ src/components/ResumeContent.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="resume-content">
+    <div class="card row">
+      <div class="card-body">
+        <h3 class="card-title">Languages 🦜</h3>
+      <ul>
+        <li>TypeScript</li>
+        <li>Node.js/JavaScript(ES6)</li>
+        <li>Go (occasional)</li>
+        <li>SQL</li>
+        <li>HTML/CSS</li>
+      </ul>
+      </div>
+    </div>
+    <br />
+    <br />
+    <div class="card row">
+      <div class="card-body">
+      <h3 class="card-title">Skills 🌱</h3>
+      <div class="row">
+        <div class="col">
+      <h3><small class="text-muted">Back End</small></h3>
+      <ul>
+        <li>API Design</li>
+        <li>
+          Relational Databases
+          <ul>
+            <li><small>MySQL</small></li>
+            <li><small>PostgreSQL</small></li>
+          </ul>
+        </li>
+        <li>
+          NoSQL
+          <ul>
+            <li><small>Redis</small></li>
+          </ul>
+        </li>
+        <li>Serverless Computing</li>
+        <li>Cloud Architecture / DevOps
+          <ul>
+            <li><small>AWS</small></li>
+            <li><small>Terraform</small></li>
+          </ul>
+        <li>Docker</li>
+        <li>CI</li>
+        <li>Unit Testing</li>
+      </ul>
+        </div>
+        <div class="col">
+      <h3><small class="text-muted">Front-end Frameworks</small></h3>
+      <ul>
+        <li>Vue</li>
+        <li>React</li>
+        <li>AngularJS</li>
+      </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+    <br />
+    <br />
+    <div class="card row">
+      <div class="card-body">
+      <h3 class="card-title">Certifications 🏆</h3>
+      <ul>
+        <li>
+          <a
+            href="https://www.youracclaim.com/badges/5b5bc9f1-7cdf-4a00-b9f0-3cf22ac50848/public_url"
+            >AWS Certified Solutions Architect – Associate</a>
+          (February 2020)
+        </li>
+      </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResumeContent',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
